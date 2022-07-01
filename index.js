02542d968d3bdd5f45fe39f0345f73a348194d57
chhavid <99489321+chhavid@users.noreply.github.com>
2022-07-01 10:07:28 +0530

    Added bodyParser handler

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { createHandler } = require('./src/server/router.js');
 const { startServer } = require('./src/server/server.js');
 const { notFound } = require('./src/handlers/notFound.js');
-const { serveFileContent } = require('./serveStaticFile.js');
+const { serveFileContent } = require('./src/handlers/serveStaticFile.js');
+const { serveFileContent } = require('./src/handlers/serveStaticFile.js');
+const { bodyParser } = require('./src/handlers/bodyParser.js');
 
-module.exports = { createHandler, startServer, notFound, serveFileContent };
+module.exports = { createHandler, startServer, notFound, serveFileContent, bodyParser };
