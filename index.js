c14194a34bfb4113931381a29e0a290bdfdac394
chhavid <99489321+chhavid@users.noreply.github.com>
2022-07-01 10:25:25 +0530

    Modified router

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { createHandler } = require('./src/server/router.js');
+const { createRouter } = require('./src/server/router.js');
 const { startServer } = require('./src/server/server.js');
 const { notFound } = require('./src/handlers/notFound.js');
 const { serveFileContent } = require('./src/handlers/serveStaticFile.js');
 const { serveFileContent } = require('./src/handlers/serveStaticFile.js');
 const { bodyParser } = require('./src/handlers/bodyParser.js');
 
-module.exports = { createHandler, startServer, notFound, serveFileContent, bodyParser };
+module.exports = { createRouter, startServer, notFound, serveFileContent, bodyParser };
--- src/server/router.js
+++ src/server/router.js
@@ -2,19 +2,26 @@ const matches = function (method, path) {
   return this.method === method && this.url.pathname === path;
 };
 
-const createHandler = (handlers) => {
+const createNext = handlers => {
+  let index = -1;
+  const callNextHandler = (req, res) => {
+    index++;
+    const currentHandler = handlers[index];
+    if (currentHandler) {
+      currentHandler(req, res, () => callNextHandler(req, res));
+    }
+  };
+  return callNextHandler;
+};
+
+const createRouter = (handlers) => {
   return (request, response) => {
     console.log(request.method, request.url);
     request.url = new URL(request.url, `http://${request.headers.host}`);
     request.matches = matches.bind(request);
-
-    for (const handler of handlers) {
-      if (handler(request, response)) {
-        return true;
-      }
-    }
-    return false;
-  }
+    const next = createNext(handlers);
+    next(request, response);
+  };
 };
 
-module.exports = { createHandler };
+module.exports = { createRouter };
