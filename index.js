b05bcd63e03af753cd10d48d7e0b207cd00d580e
chhavid <99489321+chhavid@users.noreply.github.com>
2022-07-14 14:41:54 +0530

    Modified index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { createRouter } = require('./src/server/router.js');
 const { startServer } = require('./src/server/server.js');
 const { notFound } = require('./src/handlers/notFound.js');
 const { serveFileContent } = require('./src/handlers/serveStaticFile.js');
-const { serveFileContent } = require('./src/handlers/serveStaticFile.js');
 const { bodyParser } = require('./src/handlers/bodyParser.js');
 
 module.exports = { createRouter, startServer, notFound, serveFileContent, bodyParser };
